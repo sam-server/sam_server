b4f84a3b4720e5147de397a4d130424d4a7170a4
Thomas Stephenson <ovangle@gmail.com>
2014-11-17 00:43:17 +1100

    url fixes
    Can serve .dart files in a dev environment
    Added authentication urls

--- sam_server/sam_server/urls.py
+++ sam_server/sam_server/urls.py
@@ -4,21 +4,31 @@ from django.contrib import admin
 import django.contrib.staticfiles.views as staticfiles
 
 from . import settings
+import artist.urls
+import authentication.urls
+
+def serve_dart_file(request, path):
+    ## Always serve the compiled js file
+    response = staticfiles.serve(request, path)
+    response['Content-Type'] = 'application/dart';
+    return response
 
 def serve_package(request, path):
-    print('Serving package path: {0}'.format(path))
-    return staticfiles.serve(request, path)
+    response = staticfiles.serve(request, path)
+    if path.endswith('.dart'):
+        response['Content-Type'] = 'application/dart'
+    return response
 
 def serve_css(request, path):
     print('Serving css file: {0} '.format(path))
     return staticfiles.serve(request, path)
-
 
 ## Hacks to get dart working in the development environment
 ## TODO: Should be handled better than this.
 if settings.DEBUG:
     urlpatterns = patterns('',
         url(r'^.*(?P<path>packages/.*)$', serve_package),
+        url(r'^(?P<path>.*?\.dart)$', serve_dart_file),
         url(r'^(?P<path>css/.*\.css)$', serve_css),
     )
 else:
@@ -30,7 +40,8 @@ urlpatterns += patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^artist/', include('artist.urls')),
+    url(r'^artist/', include(artist.urls)),
+    url(r'^auth/', include(authentication.urls))
 
 )
 
