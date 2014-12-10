711ef6defed4e96a3714a5a5d42470c1eff2466a
Thomas Stephenson <ovangle@gmail.com>
2014-12-11 01:41:50 +1100

    Added ext_utils.dart and ext_utils.html packages

---
+++ sam_server/ext_utils/__init__.py
@@ -0,0 +1,6 @@
+"""
+ext_utils
+=========
+
+A collection of utility packages for working with various file formats
+"""
---
+++ sam_server/ext_utils/html/render.py
@@ -0,0 +1,31 @@
+# Render a html file at a given location, using pystache
+
+import pystache
+from pystache.parser import parse
+
+_PRECOMPILED_TEMPLATES = dict()
+
+# Use ASP style delimiters so that they don't conflict with the
+# native
+DELIMITERS = ('<%', '%>')
+
+
+def _load_template(path_to_template):
+    """
+    Loads the template from the given location on the file system
+    parsed templates are cached for future access
+    """
+    if path_to_template not in _PRECOMPILED_TEMPLATES:
+        with open(path_to_template) as f:
+            template_str = f.read()
+        _PRECOMPILED_TEMPLATES[path_to_template] = parse(template_str, delimiters=DELIMITERS)
+    return _PRECOMPILED_TEMPLATES[path_to_template]
+
+
+def render(path_to_template, context=None, **kwargs):
+    template = _load_template(path_to_template)
+    return pystache.render(template, context, **kwargs)
+
+
+
+
