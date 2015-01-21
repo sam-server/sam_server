e221f7198cb6b203701dc765f287797647dafa2b
Thomas Stephenson <ovangle@gmail.com>
2015-01-21 21:42:25 +1100

    Add auto reload functionality...
    When settings.DEBUG and the code has changed, the wsgi client should
    reload the app.

--- sam_server/sam_server/wsgi.py
+++ sam_server/sam_server/wsgi.py
@@ -8,7 +8,20 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sam_server.settings_prod")
+import uwsgi
+from uwsgidecorators import timer
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sam_server.settings")
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from django.utils import autoreload
+
 application = get_wsgi_application()
+
+
+@timer(3)
+def change_code_graceful_reload(sig):
+    if settings.DEBUG and autoreload.code_changed():
+        print('Code changed. Reloading...')
+        uwsgi.reload()
