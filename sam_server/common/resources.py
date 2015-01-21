f4d449a83464bef84fb1ac31a2ec8bfc4b77d731
Thomas Stephenson <ovangle@gmail.com>
2015-01-21 18:36:55 +1100

    Changed wire format of money resource

--- sam_server/common/resources.py
+++ sam_server/common/resources.py
@@ -1,34 +1,32 @@
+import re
 
 from ext_utils.json.resources import Resource
 from .currency import MoneyAmount
 
 
 class MoneyAmountResource(Resource):
+    CURRENCY_PATTERN = re.compile(
+        r'([-+]?\d+\.\d+) ([A-Z]{3})'
+    )
+
     def to_json(self, value):
         self.check_mandatory(value)
         if value is None:
             return None
         if not isinstance(value, MoneyAmount):
             raise TypeError('Not a MoneyAmount: {0}'.format(value))
-        return {
-            'code': value.code,
-            'symbol': value.currency.symbol,
-            'num_decimal_digits': value.currency.num_digits_in_minor_units,
-            'value': value.decimal_string_value
-        }
+        return '{0} {1}'.format(value.decimal_string_value, value.code)
 
     def to_python(self, resource):
         self.check_mandatory(resource)
         if resource is None:
             return None
         if isinstance(resource, str):
-            return MoneyAmount.parse(resource)
-        elif isinstance(resource, dict):
-            try:
-                code = resource['code']
-                value_decimal_string = resource['value']
-            except KeyError:
-                raise ValueError('Invalid money amount: {0}'.format(resource))
-            return MoneyAmount(code, value_decimal_string)
+            match = MoneyAmountResource.CURRENCY_PATTERN.match(resource)
+            if match is None:
+                raise ValueError('Invalid format for money value: {0}'.format(resource))
+            value = '{0} {1}'.format(match.group(2), match.group(1))
+            return MoneyAmount.parse(value)
+        raise ValueError('Expected a string: {0}'.format(resource))
 
 
