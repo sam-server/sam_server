e654ee1bf54551e2a8d77184e9b3fe9236ef36c2
Thomas Stephenson <ovangle@gmail.com>
2015-01-27 07:17:16 +1100

    url patterns
    - media files are matched before any other content in debug mode
    - added svg content (again in debug mode)
    - changed deprecated patterns to use []

--- sam_server/sam_server/urls.py
+++ sam_server/sam_server/urls.py
@@ -12,6 +12,7 @@ from ext_utils.html import render, serve_html_file
 from ext_utils.dart import serve_dart_file
 from ext_utils.js import serve_js_file
 from ext_utils.css import serve_css_file
+from ext_utils.svg import serve_svg_file
 
 import artist.urls
 import authentication.urls
@@ -26,35 +27,39 @@ def serve_index(request):
     """
     if request.user.is_anonymous():
         return HttpResponseRedirect('/auth/login')
-    context = {
-        'init_url':  '/asset'.format(request.user.id),
-    }
-    context.update(csrf(request))
-    rendered_template = render('index.html', context)
-    return HttpResponse(rendered_template)
+    return HttpResponseRedirect('/asset')
+    #context = {
+    #    'init_url':  '/asset'.format(request.user.id),
+    #}
+    #context.update(csrf(request))
+    #rendered_template = render('index.html', context)
+    #return HttpResponse(rendered_template)
+
+
+## TODO: /media files need to be added to nginx config
+if settings.DEBUG:
+    urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns = []
 
 
 ## Hacks to get dart working in the development environment
 ## TODO: Should be handled better than this.
 if settings.DEBUG:
-    urlpatterns = patterns('',
+    urlpatterns += [
         url(r'^(?P<path>.*?\.dart)$', serve_dart_file),
         url(r'^(?P<path>.*?\.html)$', serve_html_file),
         url(r'^(?P<path>.*?\.js)$', serve_js_file),
         url(r'^(?P<path>.*?\.css)$', serve_css_file),
-    )
+        url(r'^(?P<path>.*?\.svg)$', serve_svg_file),
+    ]
 else:
-    urlpatterns = patterns('')
+    urlpatterns = []
 
-urlpatterns += patterns('',
-    # Examples:
-    # url(r'^$', 'sam_server.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns += [
     url(r'^$', serve_index),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^artist/', include(artist.urls)),
     url(r'^auth/', include(authentication.urls)),
     url(r'^asset', include(asset.urls))
-)
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
