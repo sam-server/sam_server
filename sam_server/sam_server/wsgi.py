1323c80feb20611a5ba9e22223e915074b705e51
Thomas Stephenson <ovangle@gmail.com>
2015-01-28 12:43:58 +1100

    Auto-reload changed files when running under nginx+uwsgi

--- sam_server/sam_server/wsgi.py
+++ sam_server/sam_server/wsgi.py
@@ -8,8 +8,6 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-import uwsgi
-from uwsgidecorators import timer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sam_server.settings")
 
@@ -20,8 +18,17 @@ from django.utils import autoreload
 application = get_wsgi_application()
 
 
-@timer(3)
-def change_code_graceful_reload(sig):
-    if settings.DEBUG and autoreload.code_changed():
-        print('Code changed. Reloading...')
-        uwsgi.reload()
+HAS_WSGI = False
+try:
+    import uwsgi
+    from uwsgidecorators import timer
+
+    @timer(3)
+    def change_code_graceful_reload(sig):
+        if settings.DEBUG and autoreload.code_changed():
+            print('Code changed. Reloading...')
+            uwsgi.reload()
+except ImportError:
+    ## uwsgi module only available when running
+    ## as the uwsgi server
+    pass
