b444b7f978792adcee083e83a9ccac149d2e8e39
Thomas Stephenson <ovangle@gmail.com>
2015-02-10 18:03:19 +1100

    Replaced old 'patterns' syntax for urlpatterns

--- sam_server/asset/urls.py
+++ sam_server/asset/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import patterns, url
 
 from . import views
 
-urlpatterns = patterns('assets',
+urlpatterns = [
     url(r'^$', views.list),
     url(r'^/create$', views.create),
-
     url(r'^/(?P<asset_id>[\da-fA-F]+)$', views.update_or_view)
-)
+]
